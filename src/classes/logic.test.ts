458f4caac78a2edba41c633de0e3a6b5c0613431
apanichkina <annjellyiu5@gmail.com>
2018-08-22 10:35:30 +0300

    modes for evaluate

--- src/classes/logic.test.ts
+++ src/classes/logic.test.ts
@@ -45,3 +45,41 @@ test('evaluate_common', () => {
     },
   ).consumedEnergy.value).toBe(0.320);
 });
+
+test('evaluate_day', () => {
+  const manager = new LogicManager(24);
+
+  const rates = [
+    {from: 21, to: 7, value: 1},
+    {from: 7, to: 21, value: 3},
+  ];
+
+  expect(manager.Evaluate(
+    {
+      devices: [
+        {id: '', name: '', mode: 'day', power: 1000, duration: 1},
+      ],
+      maxPower: 1000,
+      rates,
+    },
+  ).consumedEnergy.value).toBe(3); // can't set device on night period
+});
+
+test('evaluate_night', () => {
+  const manager = new LogicManager(24);
+
+  const rates = [
+    {from: 21, to: 7, value: 3},
+    {from: 7, to: 21, value: 1},
+  ];
+
+  expect(manager.Evaluate(
+    {
+      devices: [
+        {id: '', name: '', mode: 'night', power: 1000, duration: 1},
+      ],
+      maxPower: 1000,
+      rates,
+    },
+  ).consumedEnergy.value).toBe(3); // can't set device on day period
+});
